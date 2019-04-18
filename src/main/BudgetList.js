c4882943826a2a00986e1382b07df78108abfa4d
EvyHaan <evy.haan@gmail.com>
2019-04-17 17:38:38 -0700

    navigating past login page to list of budgets.

--- src/main/BudgetList.js
+++ src/main/BudgetList.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import BudgetDetail from './BudgetDetail';
 
+// export default (props) => {
+//         return (
+//         <ul>
+//             {props.budgets.map((budget) => {
+//                 return <li key={budget.id}>
+//                     {budget.name}
+//                 </li>
+//             })}
+//         </ul>)
+// }
+
+
 export default (props) => (
-        <ul>
-            {props.map((budget) => (
-                <li key={budget.id}>
-                    {budget.name}
-                </li>
-            ))}
-        </ul>
+    <ul>
+        {props.budets.map(budget => (
+            <BudgetDetail key={budget.id} budget={budget} />
+        ))}
+    </ul>
 )
